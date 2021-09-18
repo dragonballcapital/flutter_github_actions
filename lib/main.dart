813bcb3b5ce40d890ef15d466fc5031b80848d21
DragonBall <dev@dragonballcapital.net>
2021-09-18 22:00:30 +0800

    Update title and color

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'A Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primaryColor: Colors.black,
+        primarySwatch: Colors.teal,
       ),
       home: const MyHomePage(title: 'Flutter & Github Actions Demo'),
     );
